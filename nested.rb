00f3acbc5f3afb5fa0526e6a8182451a20111d27
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-24 01:11:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
      }
      
      dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
-     puts dennis_ritchies_language
+     dennis_ritchies_language.first
 end
 
 def adding_matz
