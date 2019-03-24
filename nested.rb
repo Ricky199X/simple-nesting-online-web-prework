757293a0c3b91292e225968a8795f8605bfd0189
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-24 01:02:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:alan_kay][:languages]
 end
 
 def dennis_ritchies_language
